7ae94db3313e7f62da8ffe4d491b6c771f7c0fcd
Ben <ben.pearce@sky.com>
2021-02-16 18:17:03 +0000

    Add front-end config file

---
+++ src/museum.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiBase: 'http://localhost:8000/',
+  site: {
+    name: 'Virtual Museum',
+    tagline: 'All your favourite museum objects in one place!'
+  },
+  results: {
+    resultsPerPage: 10
+  }
+};
