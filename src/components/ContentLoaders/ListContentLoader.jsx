f30772f0974b5ffe6eb1dbe9239f9a1f873c158c
Ben <ben.pearce@sky.com>
2021-02-17 19:13:09 +0000

    Add list view content skeleton loader

---
+++ src/components/ContentLoaders/ListContentLoader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const ListContentLoader = () => (
+  <ContentLoader 
+    speed={2}
+    width="100%"
+    height="100%"
+    viewBox="0 0 550 130"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <circle cx="20" cy="20" r="20" /> 
+    <rect x="0" y="0" rx="10" ry="10" width="130" height="114" /> 
+    <rect x="140" y="6" rx="0" ry="0" width="358" height="20" /> 
+    <rect x="140" y="37" rx="0" ry="0" width="156" height="14" /> 
+    <rect x="140" y="58" rx="0" ry="0" width="61" height="14" />
+  </ContentLoader>
+);
+
+export default ListContentLoader;
