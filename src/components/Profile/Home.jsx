12b8236847856ee90ada841d4e4c1495c13bd2cf
Ben <ben.pearce@sky.com>
2021-03-03 19:17:50 +0000

    Profile overview component

---
+++ src/components/Profile/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Helmet from 'react-helmet';
+
+import Alert from 'react-bootstrap/Alert';
+import Card from 'react-bootstrap/Card';
+
+import { UserContext } from '../User/AuthProvider';
+
+import Config from '../../museum.config';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <UserContext.Consumer>
+        {({ user }) => 
+          <>
+            <Helmet>
+              <title>{`Profile - ${Config.site.name}`}</title>
+            </Helmet>
+            <Alert variant='success'>
+              <Alert.Heading>Hey {user.firstName}, welcome to your <strong>profile</strong>!</Alert.Heading>
+              <p>
+                You can see details about yourself and view and manage your favourites here.
+              </p>
+            </Alert>
+            <Card>
+              <Card.Body>
+                <p><strong>User ID: </strong></p> <pre>{user.id}</pre>
+                <hr/>
+                <p><strong>First Name: </strong> {user.firstName}</p>
+                <p><strong>Last Name: </strong> {user.lastName}</p>
+                <hr/>
+                <p><strong>Email Address: </strong> {user.email}</p>
+              </Card.Body>
+            </Card>
+          </>}
+      </UserContext.Consumer>
+    );
+  }
+}
+
+export default Home;
