aaad35d5b8fa6b2e08b916deb8aa911f0428d647
Ben <ben.pearce@sky.com>
2021-03-10 12:58:09 +0000

    Fix share button URL

--- src/components/Item/ShareButton.jsx
+++ src/components/Item/ShareButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Popover from 'react-bootstrap/Popover';
@@ -13,42 +13,45 @@ import {
   faClipboard
 } from '@fortawesome/free-solid-svg-icons';
 
-const sharePopover = (
-  <Popover>
-    <Popover.Title as='h3'>Share</Popover.Title>
-    <Popover.Content>
-      <InputGroup>
-        <FormControl
-          readOnly
-          id='sharePageUrl'
-          aria-label='Page URL'
-          value={window.location.href}
-        />
-        <InputGroup.Append>
-          <OverlayTrigger
-            trigger={['hover', 'click']}
-            placement='bottom'
-            overlay={<Tooltip>Copy to clipboard</Tooltip>}
-          >
-            <Button 
-              variant='outline-secondary'
-              onClick={() => {
-                const shareUrlInput = document.getElementById('sharePageUrl');
-                shareUrlInput.select();
-                document.execCommand('copy');
-
-              }}
-            > <FontAwesomeIcon icon={faClipboard}/></Button>
-          </OverlayTrigger>
-        </InputGroup.Append>
-      </InputGroup>
-    </Popover.Content>
-  </Popover>
-);
-
 const ShareButton = () => {
+  const [url, setUrl] = useState(window.location.href);
+
   return (
-    <OverlayTrigger trigger='click' placement='bottom' overlay={sharePopover}>
+    <OverlayTrigger 
+      trigger='click' 
+      placement='bottom' 
+      onToggle={() => setUrl(window.location.href)}
+      overlay={
+        <Popover>
+          <Popover.Title as='h3'>Share</Popover.Title>
+          <Popover.Content>
+            <InputGroup>
+              <FormControl
+                readOnly
+                id='sharePageUrl'
+                aria-label='Page URL'
+                value={url}
+              />
+              <InputGroup.Append>
+                <OverlayTrigger
+                  trigger={['hover', 'click']}
+                  placement='bottom'
+                  overlay={<Tooltip>Copy to clipboard</Tooltip>}
+                >
+                  <Button 
+                    variant='outline-secondary'
+                    onClick={() => {
+                      const shareUrlInput = document.getElementById('sharePageUrl');
+                      shareUrlInput.select();
+                      document.execCommand('copy');
+                    }}
+                  > <FontAwesomeIcon icon={faClipboard}/></Button>
+                </OverlayTrigger>
+              </InputGroup.Append>
+            </InputGroup>
+          </Popover.Content>
+        </Popover>
+      }>
       <Button 
         size='sm' 
         variant='outline-dark' 
