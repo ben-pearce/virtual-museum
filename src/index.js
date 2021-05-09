d93ca29d7050e42b74d26f54d0964f79885a6e7f
Ben <ben.pearce@sky.com>
2021-05-09 18:16:20 +0100

    JSDoc documentation: Application entry-point

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+/**
+ * Main entry-point for react application, mounts the {@link App} ReactNode into
+ * the DOM.
+ */
 ReactDOM.render( 
   <App />,
   document.getElementById('root')
