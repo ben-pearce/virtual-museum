6fe361f080e56cdd78f21838614ff70cebbd3b16
Ben <ben.pearce@sky.com>
2021-05-09 18:14:58 +0100

    JSDoc documentation: Image preloader

--- src/imagePreloader.js
+++ src/imagePreloader.js
@@ -1,13 +1,37 @@
 
+/**
+ * Image preloader class used for copying <i>n</i> images into browser memory
+ * and providing direct access to image instances.
+ */
 class ImagePreloader {
 
+  /**
+   * Create an image preloader.
+   */
   constructor() {
     this._imageCache = null;
+
+    /**
+     * Images pre-loaded or null before {@link ImagePreloader#load} is callled.
+     * @member {Image[]|null} 
+     */
     this.images = null;
 
     this._reset();
   }
 
+  /**
+   * Starts the pre-loading asynchronously.
+   * 
+   * @example
+   * let imageUrls = [ ... ];
+   * let preloader = new ImagePreloader();
+   * let images = await preloader.load(imageUrls);
+   * 
+   * @param {string[]} imageUrls The image URLs to be preloaded.
+   * @returns {Promise<Image[]>} A promise that resolves to the pre-loaded
+   * images.
+   */
   load(imageUrls) {
     return new Promise((resolve) => {
       for(const imageUrl of imageUrls) {
@@ -29,16 +53,32 @@ class ImagePreloader {
     });
   }
 
+  /**
+   * Flush image cache to public access once complete.
+   * 
+   * @private
+   */
   _flush() {
     this.images = this._imageCache;
     this._imageCache = [];
   }
 
+  /**
+   * Reset the cache and image store for re-use.
+   * 
+   * @private
+   */
   _reset() {
     this.images = [];
     this._imageCache = [];
   }
 
+  /**
+   * Invoked when an image loads to check if pre-loading has completed.
+   * @returns {boolean} True if pre-loading is complete.
+   * 
+   * @private
+   */
   _allImagesLoaded() {
     for(const image of this._imageCache) {
       if(!image.complete) {
